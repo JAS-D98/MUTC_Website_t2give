7a9cd8c531d7eeb1385bef1016fb20ea62d1bd4c
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-06-02 22:30:27 +0300

    make home page responsive

---
+++ src/components/LeadersCard/LeadersCard.js
@@ -0,0 +1,9 @@
+import Testimonial1 from './../../assets/images/Testimonial1.png'
+import Testimonial2 from './../../assets/images/Testimonial1.png'
+import Testimonial3 from './../../assets/images/Testimonial1.png'
+import Testimonial4 from './../../assets/images/Testimonial1.png'
+import Testimonial5 from './../../assets/images/Testimonial1.png'
+
+const leadersCards=[
+    {image:Testimonial1,  }
+]

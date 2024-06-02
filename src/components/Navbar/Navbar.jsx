96e3ad1b08e223258f7073fc7d8bd2e86b5236e9
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-06-02 23:33:41 +0300

    add initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -15,6 +15,16 @@ const Menu=()=>{
        </div>
     )
 }
+
+// const Menu =()=>(
+//     <>
+//     <p><a href="#home">Home</a></p>
+//     <p><a href="#wgpt3">What is GPT3?</a></p>
+//     <p><a href="#possibility">Open AI</a></p>
+//     <p><a href="#features">Case Studies</a></p>
+//     <p><a href="#blog">Library</a></p>
+//     </>
+//      )
 export default function Navbar() {
   return (
    <div className="header-el">

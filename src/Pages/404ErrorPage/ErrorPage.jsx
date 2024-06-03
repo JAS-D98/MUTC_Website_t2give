488813b4a7d297091064b70e9278dc4fb0b1c5db
Jasper Ngunjiri <jasperwambugu@gmail.com>
2024-06-03 15:47:16 +0300

    reduce width of leadership card

--- src/Pages/404ErrorPage/ErrorPage.jsx
+++ src/Pages/404ErrorPage/ErrorPage.jsx
@@ -4,7 +4,7 @@ import error from './../../assets/images/error.gif'
 export default function ErrorPage() {
   return (
     <div className='error'>
-        <img src={error} alt="gif of a 404 error"/>
+        <img src={error} alt="gif of a 404 error" width="100%"/>
         <h1>OOP's Page Not Found</h1>
     </div>
   )
